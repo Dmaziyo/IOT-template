34d26ae9c4af428ee674d5e43bcce467cbff5e03
D城兔Maziyo <247948681@qq.com>
2022-09-02 21:21:05 +0800

    fix:custom service

--- my-project/src/api/worker/services/worker.js
+++ my-project/src/api/worker/services/worker.js
@@ -7,9 +7,11 @@ const { createCoreService } = require("@strapi/strapi").factories;
 
 module.exports = createCoreService("api::worker.worker", ({ strapi }) => ({
   findCard: async (card_num) => {
-    let result = await strapi.entityService.findOne("api::worker.worker", 1, {
-      populate: { card_num },
-    });
+    let result = await strapi.entityService.findOne(
+      "api::worker.worker",
+      1,
+      this.getFetchParams({ card_num })
+    );
     return result;
   },
 }));
