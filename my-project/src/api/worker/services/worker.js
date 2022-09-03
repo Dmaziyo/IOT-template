4453c21f4147dc4d83c07b71d8f5dc40186919eb
D城兔Maziyo <247948681@qq.com>
2022-09-03 09:55:55 +0800

    add custom service

--- my-project/src/api/worker/services/worker.js
+++ my-project/src/api/worker/services/worker.js
@@ -7,11 +7,9 @@ const { createCoreService } = require("@strapi/strapi").factories;
 
 module.exports = createCoreService("api::worker.worker", ({ strapi }) => ({
   findCard: async (card_num) => {
-    let result = await strapi.entityService.findOne(
-      "api::worker.worker",
-      1,
-      this.getFetchParams({ card_num })
-    );
+    let result = await strapi.db.query("api::worker.worker").findOne({
+      where: { card_num },
+    });
     return result;
   },
 }));
