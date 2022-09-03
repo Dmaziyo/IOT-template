3d3cddd2ada199646470561615312ca73b600b0f
D城兔Maziyo <247948681@qq.com>
2022-09-03 09:55:36 +0800

    fix:custom routes

--- my-project/src/api/worker/routes/worker.js
+++ my-project/src/api/worker/routes/worker.js
@@ -6,12 +6,4 @@
 
 const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter("api::worker.worker", {
-  config: {
-    create: {
-      // disables authorization requirement for the `find` route
-      policies: ["admin::isAuthenticatedAdmin"],
-      // here you can also customize auth & middlewares
-    },
-  },
-});
+module.exports = createCoreRouter("api::worker.worker");
